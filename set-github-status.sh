e8a766b51573bf2beeecbd1de350f4754bb6eb6a
Romain Grecourt <romain.grecourt@oracle.com>
2017-12-18 20:41:54 -0800

    updates

--- set-github-status.sh
+++ set-github-status.sh
@@ -1,14 +1,25 @@
 #!/bin/bash -ex
 
-# TODO get gitlab pipeline status
-cat > status.json <<
-{
-  "state": "success",
-  "target_url": "${CI_PROJECT_URL}/pipelines/${CI_PIPELINE_ID}",
-  "description": "The pipeline succeeded!",
-  "context": "internal/gitlab-pipeline"
-}
-EOF
+if [ -z "${1}" = "pending" ] ; then
+  cat > status.json <<
+  {
+    "state": "pending",
+    "target_url": "${CI_PROJECT_URL}/pipelines/${CI_PIPELINE_ID}",
+    "description": "Internal Gitlab pipeline created",
+    "context": "internal/gitlab-pipeline"
+  }
+  EOF
+else
+  # TODO get gitlab pipeline status
+  cat > status.json <<
+  {
+    "state": "success",
+    "target_url": "${CI_PROJECT_URL}/pipelines/${CI_PIPELINE_ID}",
+    "description": "Internal Gitlab pipeline passed",
+    "context": "internal/gitlab-pipeline"
+  }
+  EOF
+fi
 
 curl \
   -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
